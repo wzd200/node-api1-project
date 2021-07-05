ca3de1a8e5b65280321a6506c6dd46968f1c7179
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-07-05 16:53:49 -0400

    All tests passing

--- api/server.js
+++ api/server.js
@@ -1,3 +1,108 @@
-// BUILD YOUR SERVER HERE
+const express = require('express')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const User = require('./users/model')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'operational'})
+})
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message
+            })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    User.findById(id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: 'The user with the specified ID does not exist'
+                })
+            } else {
+                res.status(200).json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'The user information could not be retrieved',
+                error: err.message
+            })
+        })
+})
+
+server.post('/api/users', (req, res) => {
+    if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    } else {
+        const { name, bio } = req.body
+        User.insert({ name, bio })
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'There was an error while saving the user to the database',
+                    error: err.message
+                })
+            })
+    }
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    const { name, bio } = req.body
+    if (!name || !bio) {
+        res.status(400).json({ message: 'Please provide name and bio for the user' })
+    } else {
+        User.update(id, { name, bio })
+            .then(updated => {
+                if (!updated) {
+                    res.status(404).json({ message: 'The user with the specified ID does not exist' })
+                } else {
+                    res.json(updated)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'The user information could not be modified',
+                    error: err.message,
+                })
+            })
+    }
+  })
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    User.remove(id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: 'The user with the specified ID does not exist'
+                })
+            } else {
+                res.status(200).json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'The user could not be removed',
+                error: err.message
+            })
+        })
+})
+
+module.exports = server
